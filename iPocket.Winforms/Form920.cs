1412e7917307aaee75b114ddfea3834e2855474f
Sushi Roll <gabb09.perez@gmail.com>
2026-05-08 18:45:06 +0800

    Add files via upload

---
+++ iPocket.Winforms/Form920.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace iPocket
+{
+    public partial class Form920 : Form
+    {
+        public Form920()
+        {
+            InitializeComponent();
+        }
+    }
+}
